7e2e146721c105ebb5344e017a9daeb9713a817d
Uli Köhler <ulikoehler@online.de>
2013-02-20 12:20:51 +0100

    Fix strange stuff

--- StrangeStuff/lwp.pl
+++ StrangeStuff/lwp.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl -w
 use LWP::Simple;
-my $a = get "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt"
+my $a = get "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt";
 print $a;
