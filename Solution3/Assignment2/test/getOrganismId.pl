7bc1631ab3b647ed3c6abc6590b2aa0d5dd77493
Uli Köhler <ulikoehler@online.de>
2013-02-22 01:28:06 +0100

    Added test code

--- Solution3/Assignment2/test/getOrganismId.pl
+++ Solution3/Assignment2/test/getOrganismId.pl
@@ -3,23 +3,24 @@ use DBI;
 #Get the organism ID, insert it if not existent
 sub getOrganismId {
 	my $db = $_[0];
-	my $organismName = $_[1];
+	my $organism = $_[1];
 	#Try to fetch an existing organism
-	$db->do("SELECT Id from Organism WHERE Name = ?;", undef, $organism);
-	my $result = $sth->fetchrow_hashref();
+	my $queryOrganismIdStmt = $db->prepare("SELECT Id from Organism WHERE Name = ?;");
+	$queryOrganismIdStmt->execute($organism);
+	my $result = $queryOrganismIdStmt->fetchrow_hashref();
 	if(defined $result->{Id}) {
 		return $result->{Id}
 	}
 	#If that didn't return, we need to insert it
-	$db->do("INSERT INTO Organism('Name') VALUES (?);", undef, $organism);
+	my $insertStmt = $db->prepare("INSERT INTO Organism(`Name`) VALUES (?)");
+	$insertStmt->execute($organism);
+	$autoIncrementId = $insertStmt->{mysql_insertid};
 	#Fetch it again (there may be a better way)
-	$db->do("SELECT Id from Organism WHERE Name = ?;", undef, $organism);
-	my $result = $sth->fetchrow_hashref();
-	if(defined $result->{Id}) {
-		return $result->{Id}
-	}
+	return $autoIncrementId;
 }
 
 $db = DBI->connect('DBI:mysql:bioprakt4;host=localgrid.de', 'bioprakt4', 'vGI5GCMg0x') || die "Could not connect to database: $DBI::errstr";
 $id = getOrganismId($db, "Escherichia Coli sp. Strain 4");
+print "Got ID $id\n";
+$id = getOrganismId($db, "Escherichia Coli sp. Strain 5");
 print "Got ID $id\n";
