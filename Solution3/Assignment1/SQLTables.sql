5f7771adcf838fb2b42c8c7b05239f622a5ca83a
quant <quant@quant-TOSHIBA-NB550D.(none)>
2013-02-22 02:20:57 +0100

    Id on foreign keys added

--- Solution3/Assignment1/SQLTables.sql
+++ Solution3/Assignment1/SQLTables.sql
@@ -26,23 +26,23 @@ CREATE TABLE Type (
 );
 CREATE TABLE SeqDBEntry (
     Id INT NOT NULL AUTO_INCREMENT,
-    SeqDB INT,
+    SeqDBId INT,
     SeqDBIdentifier VARCHAR(50),
     PRIMARY KEY(Id),
-    FOREIGN KEY(SeqDB) references SeqDB(Id)
+    FOREIGN KEY(SeqDBId) references SeqDB(Id)
 );
 CREATE TABLE Seq (
     Id INT NOT NULL AUTO_INCREMENT,
     Name VARCHAR(50),
     Definition TEXT,
     Seq LONGTEXT,
-    Organism INT,
+    OrganismId INT,
     TypeId INT,
-    secStruct INT,
+    secStructId INT,
     PRIMARY KEY(Id),
-    FOREIGN KEY(Organism) references Organism(Id),
+    FOREIGN KEY(OrganismId) references Organism(Id),
     FOREIGN KEY(TypeId) references Type(Id),
-    FOREIGN KEY(secStruct) references SecondStruct(Id)
+    FOREIGN KEY(secStructId) references SecondStruct(Id)
 );
 CREATE TABLE Source (
     SeqId INT,
@@ -54,11 +54,11 @@ CREATE TABLE Source (
 CREATE TABLE StructAlign (
     Id INT NOT NULL AUTO_INCREMENT,
     Name VARCHAR(50),
-    Seq1 INT,
-    Seq2 INT,
+    Seq1Id INT,
+    Seq2Id INT,
     PRIMARY KEY(Id),
-    FOREIGN KEY(Seq1) references Seq(Id),
-    FOREIGN KEY(Seq2) references Seq(Id)
+    FOREIGN KEY(Seq1Id) references Seq(Id),
+    FOREIGN KEY(Seq2Id) references Seq(Id)
 );
 INSERT INTO SeqDB (`Name`) VALUES('SwissProt');    
 INSERT INTO Type (`Name`) VALUES('DNA');
