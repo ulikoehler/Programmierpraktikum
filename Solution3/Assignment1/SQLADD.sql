42c0fd4492b798e1c5fdf99cd97373eae87ae902
quant <quant@quant-TOSHIBA-NB550D.(none)>
2013-02-22 10:23:10 +0100

    afsjio

--- Solution3/Assignment1/SQLADD.sql
+++ Solution3/Assignment1/SQLADD.sql
@@ -1,4 +1,4 @@
-INSERT INTO SeqDB (`Name`) VALUES('SwissProt');    
+INSERT INTO SeqDB (`Name`) VALUES('SuissProt');    
 INSERT INTO Type (`Name`) VALUES('DNA');
 INSERT INTO Type (`Name`) VALUES('RNA');
 INSERT INTO Type (`Name`) VALUES('Protein');
--- Solution3/Assignment1/SQLCreateTables.sql
+++ Solution3/Assignment1/SQLCreateTables.sql
@@ -68,9 +68,3 @@ CREATE TABLE KeySeq (
     FOREIGN KEY(KeywordsId) references Keywords(Id),
     FOREIGN KEY(SeqId) references Seq(Id)
 );
-
-
-INSERT INTO SeqDB (`Name`) VALUES('SwissProt');    
-INSERT INTO Type (`Name`) VALUES('DNA');
-INSERT INTO Type (`Name`) VALUES('RNA');
-INSERT INTO Type (`Name`) VALUES('Protein');
