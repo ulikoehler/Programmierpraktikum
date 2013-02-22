3f63275df6cfe24f69bfaf49af46026f7d8c72bf
Yann Spöri <yann_spoeri@web.de>
2013-02-22 19:49:49 +0100

    New Version

--- Solution3/Assignment1/SQLCreateTables.sql
+++ Solution3/Assignment1/SQLCreateTables.sql
@@ -1,6 +1,13 @@
-CREATE TABLE SeqDB (
-    Id INTEGER NOT NULL AUTO_INCREMENT,
-    Name VARCHAR(50) NOT NULL UNIQUE,
+CREATE TABLE DB (
+  Id INTEGER NOT NULL AUTO_INCREMENT,
+  Name VARCHAR(50) NOT NULL UNIQUE,
+  SeqDBIdentifier VARCHAR(50),
+  SwissprotEntryId TEXT,
+  PRIMARY KEY(Id)
+);
+CREATE TABLE Organism (
+    Id INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(255),
     PRIMARY KEY(Id)
 );
 CREATE TABLE Keywords (
@@ -8,58 +15,24 @@ CREATE TABLE Keywords (
   Value TEXT NOT NULL,
   PRIMARY KEY(ID)
 );
-CREATE TABLE SecondStruct (
-    Id INT NOT NULL AUTO_INCREMENT,
-    Struct TEXT NOT NULL,
-    PRIMARY KEY(Id)
-);
-CREATE TABLE Organism (
-    Id INT NOT NULL AUTO_INCREMENT,
-    Name VARCHAR(255),
-    PRIMARY KEY(Id)
-);
-CREATE TABLE Type (
-  Id INT NOT NULL AUTO_INCREMENT,
-  Name VARCHAR(50),
-  Description TEXT,
-  PRIMARY KEY(Id)
-);
-CREATE TABLE SeqDBEntry (
-    Id INT NOT NULL AUTO_INCREMENT,
-    SeqDBId INT,
-    SeqDBIdentifier VARCHAR(50),
-    SwissprotEntryId TEXT,
-    PRIMARY KEY(Id),
-    FOREIGN KEY(SeqDBId) references SeqDB(Id)
-);
 CREATE TABLE Seq (
     Id INT NOT NULL AUTO_INCREMENT,
     Name VARCHAR(50),
     Definition TEXT,
     Seq LONGTEXT,
     OrganismId INT,
-    TypeId INT,
-    secStructId INT,
+    Type ENUM('RNA', 'DNA', 'Protein'),
+    Structure LONGTEXT,
+    DBId Int,
     PRIMARY KEY(Id),
     FOREIGN KEY(OrganismId) references Organism(Id),
-    FOREIGN KEY(TypeId) references Type(Id),
-    FOREIGN KEY(secStructId) references SecondStruct(Id)
-);
-CREATE TABLE Source (
-    SeqId INT,
-    EntryId Int,
-    PRIMARY KEY(SeqId, EntryId),
-    FOREIGN KEY(SeqId) references Seq(Id),
-    FOREIGN KEY(EntryId) references SeqDBEntry(Id)
+    FOREIGN KEY(DBId) references DB(Id)
 );
 CREATE TABLE StructAlign (
     Id INT NOT NULL AUTO_INCREMENT,
-    Name VARCHAR(50),
-    Seq1Id INT,
-    Seq2Id INT,
-    PRIMARY KEY(Id),
-    FOREIGN KEY(Seq1Id) references Seq(Id),
-    FOREIGN KEY(Seq2Id) references Seq(Id)
+    Seq INT,
+    PRIMARY KEY(Id, Seq),
+    FOREIGN KEY(Seq) references Seq(Id)
 );
 CREATE TABLE KeySeq (
     KeywordsId INT,
