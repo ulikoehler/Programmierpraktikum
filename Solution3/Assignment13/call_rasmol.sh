bb4c4d239845438c027ec71948e06d3b9d9b3c25
Uli Köhler <ulikoehler@online.de>
2013-02-23 18:10:59 +0100

    Added rasmol

--- Solution3/Assignment13/call_rasmol.sh
+++ Solution3/Assignment13/call_rasmol.sh
@@ -21,7 +21,7 @@ echo "png $1.png;" >> $1.pml
 #echo "quit;" >> $1.pml
 #Execute RASMOL
 export wd=`pwd`
-sh -i ~/.ssh/id_dsa koehleru@remote.cip.ifi.lmu.de "cd /home/k/koehleru/public_html/bioprakt/cgi/ && pymol -qxci 1ULI.pml"
+ssh -i ~/.ssh/id_dsa koehleru@remote.cip.ifi.lmu.de "cd /home/k/koehleru/public_html/bioprakt/cgi/ && pymol -qxci 1ULI.pml"
 mv $1.png pdbcache/
 #Remove tmp files
 #rm -rf $1.pml $1.pdb
