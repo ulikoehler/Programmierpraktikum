dceeaadcb38d21618b814116b708d5f8cb0f79e2
Uli Köhler <ulikoehler@online.de>
2013-02-23 18:39:55 +0100

    Protein rendering works nosw

--- Solution3/Assignment13/call_rasmol.sh
+++ Solution3/Assignment13/call_rasmol.sh
@@ -4,25 +4,26 @@ if [ $# -eq 0 ]
     echo "Usage: call_rasmol <PDB ID>"
     exit
 fi
-#Change working 
+#Use a different workdir. Can't use mktemp because the directory must be mounted via NFS)
 cd /home/k/koehleru/public_html/bioprakt/cgi/
+export workdir=/home/k/koehleru/public_html/bioprakt/cgi/tmp
 #Download
-wget -qO $1.pdb http://www.rcsb.org/pdb/files/$1.pdb
+wget -qO $workdir/$1.pdb http://www.rcsb.org/pdb/files/$1.pdb
 #Create the rasmol script
-echo "load $1.pdb;" > $1.pml
-echo "bg_color white;" >> $1.pml
-echo "set ray_opaque_background, on;" >> $1.pml
-echo "show cartoon;" >> $1.pml
-echo "color purple, ss h;" >> $1.pml
-echo "color yellow, ss s;" >> $1.pml
-echo "color gree, ss "";" >> $1.pml
-echo "ray 1000,1000;" >> $1.pml
-echo "png $1.png;" >> $1.pml
+echo "load $1.pdb;" > $workdir/$1.pml
+echo "bg_color white;" >> $workdir/$1.pml
+echo "set ray_opaque_background, on;" >> $workdir/$1.pml
+echo "show cartoon;" >> $workdir/$1.pml
+echo "color purple, ss h;" >> $workdir/$1.pml
+echo "color yellow, ss s;" >> $workdir/$1.pml
+echo "color gree, ss "";" >> $workdir/$1.pml
+echo "ray 1000,1000;" >> $workdir/$1.pml
+echo "png $1.png;" >> $workdir/$1.pml
 #echo "quit;" >> $1.pml
 #Execute RASMOL
-export wd=`pwd`
-ssh -i ~/.ssh/id_dsa koehleru@remote.cip.ifi.lmu.de "cd /home/k/koehleru/public_html/bioprakt/cgi/ && pymol -qxci 1ULI.pml"
-mv $1.png pdbcache/
+ssh -i ~/.ssh/id_dsa koehleru@remote.cip.ifi.lmu.de "cd $workdir && pymol -qxci $1.pml"
+sleep 0.2 #OH DUDE WE RLY HAVE TO WAIT FOR THE 
+#Move the file
+mv $workdir/$1.png pdbcache/
 #Remove tmp files
-#rm -rf $1.pml $1.pdb
-
+rm -rf $workdir/$1.pml $workdir/$1.pdb
