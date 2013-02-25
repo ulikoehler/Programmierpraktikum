1af8b6f8f4b61426f4fe4519817cd23f24c053ae
Uli Köhler <ulikoehler@online.de>
2013-02-26 00:42:05 +0100

    Stuff

--- Solution4/Web-Util/insert_autocomplete_data.pl
+++ Solution4/Web-Util/insert_autocomplete_data.pl
@@ -5,7 +5,7 @@ use DBI;
 use Getopt::Std;
 #Subs
 my $db = DBI->connect('DBI:mysql:bioprakt4;host=mysql2-ext.bio.ifi.lmu.de', 'bioprakt4', 'vGI5GCMg0x') || die "Could not connect to database: $DBI::errstr";
-my $insert = $db->prepare("INSERT DELAYED INTO IDAutocomplete (`DBId`,`Name`) SELECT Id, ? FROM DB WHERE DB.Name = ?");
+my $insert = $db->prepare("INSERT DELAYED IGNORE INTO IDAutocomplete (`DBId`,`Name`) SELECT Id, ? FROM DB WHERE DB.Name = ?");
 #PDBID
 open PDBIDS, "<data/pdbids" or die $!;
 while (<PDBIDS>) {
