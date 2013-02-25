faa37dce73c2df839099f77172a1481ad437d653
Yann Spöri <yann_spoeri@web.de>
2013-02-25 09:38:33 +0100

    Alignment Tools

---
+++ Solution4/Assignment1/Alignments/src/alignments/Alignments.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alignments;
+
+/**
+ *
+ * @author spoeri
+ */
+public class Alignments {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
