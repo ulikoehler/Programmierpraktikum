afbbb2a68311d4bae6b8e0a3ee08b5937e6fdfd7
Uli Köhler <ulikoehler@online.de>
2013-02-25 10:15:45 +0100

    Added projects

---
+++ Solution4/Validation/src/main/java/de/bioinformatikmuenchen/pg4/validation/App.java
@@ -0,0 +1,13 @@
+package de.bioinformatikmuenchen.pg4.validation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
