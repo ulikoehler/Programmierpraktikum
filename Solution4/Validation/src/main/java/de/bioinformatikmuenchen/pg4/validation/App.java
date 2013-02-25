5bc3ba616ae9179bbf33c4ee49107a9a64d92de7
Rene Schoeffel <rene@sdxc.de>
2013-02-25 20:04:05 +0100

    added speci,sensi,cover to validation

--- Solution4/Validation/src/main/java/de/bioinformatikmuenchen/pg4/validation/App.java
+++ Solution4/Validation/src/main/java/de/bioinformatikmuenchen/pg4/validation/App.java
@@ -8,6 +8,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String a1 ="VBSD__EFG";
+        String b1 ="L_MNORQR_";
+        String c1 ="_VB_SDE_FG";
+        String d1 ="LMNOP_QR__";
+        
+        String a2 ="QW_ERTA";
+        String b2 ="__MORTA";
+        String c2 ="QW__ERTA";
+        String d2 ="_MOR__TA_";
+        
+        AliValiAlg test = new AliValiAlg(a2,b2,c2,d2);
+        
+        System.out.println(test.getCover());
+        
     }
 }
