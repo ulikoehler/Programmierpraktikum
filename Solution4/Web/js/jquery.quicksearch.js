11377821d18192e9f836314e47f7fbe1aabe88e2
Uli Köhler <ulikoehler@online.de>
2013-02-25 21:44:57 +0100

    Added stuff

---
+++ Solution4/Web/js/jquery.quicksearch.js
@@ -0,0 +1,207 @@
+(function($, window, document, undefined) {
+	$.fn.quicksearch = function (target, opt) {
+		
+		var timeout, cache, rowcache, jq_results, val = '', e = this, options = $.extend({ 
+			delay: 100,
+			selector: null,
+			stripeRows: null,
+			loader: null,
+			noResults: '',
+			bind: 'keyup',
+			onBefore: function () { 
+				return;
+			},
+			onAfter: function () { 
+				return;
+			},
+			show: function () {
+				this.style.display = "";
+			},
+			hide: function () {
+				this.style.display = "none";
+			},
+			prepareQuery: function (val) {
+				return val.toLowerCase().split(' ');
+			},
+			testQuery: function (query, txt, _row) {
+				for (var i = 0; i < query.length; i += 1) {
+					if (txt.indexOf(query[i]) === -1) {
+						return false;
+					}
+				}
+				return true;
+			}
+		}, opt);
+		
+		this.go = function () {
+			
+			var i = 0, 
+			noresults = true, 
+			query = options.prepareQuery(val),
+			val_empty = (val.replace(' ', '').length === 0);
+			
+			for (var i = 0, len = rowcache.length; i < len; i++) {
+				if (val_empty || options.testQuery(query, cache[i], rowcache[i])) {
+					options.show.apply(rowcache[i]);
+					noresults = false;
+				} else {
+					options.hide.apply(rowcache[i]);
+				}
+			}
+			
+			if (noresults) {
+				this.results(false);
+			} else {
+				this.results(true);
+				this.stripe();
+			}
+			
+			this.loader(false);
+			options.onAfter();
+			
+			return this;
+		};
+		
+		this.stripe = function () {
+			
+			if (typeof options.stripeRows === "object" && options.stripeRows !== null)
+			{
+				var joined = options.stripeRows.join(' ');
+				var stripeRows_length = options.stripeRows.length;
+				
+				jq_results.not(':hidden').each(function (i) {
+					$(this).removeClass(joined).addClass(options.stripeRows[i % stripeRows_length]);
+				});
+			}
+			
+			return this;
+		};
+		
+		this.strip_html = function (input) {
+			var output = input.replace(new RegExp('<[^<]+\>', 'g'), "");
+			output = $.trim(output.toLowerCase());
+			return output;
+		};
+		
+		this.results = function (bool) {
+			if (typeof options.noResults === "string" && options.noResults !== "") {
+				if (bool) {
+					$(options.noResults).hide();
+				} else {
+					$(options.noResults).show();
+				}
+			}
+			return this;
+		};
+		
+		this.loader = function (bool) {
+			if (typeof options.loader === "string" && options.loader !== "") {
+				 (bool) ? $(options.loader).show() : $(options.loader).hide();
+			}
+			return this;
+		};
+		
+		this.cache = function () {
+			
+			jq_results = $(target);
+			
+			if (typeof options.noResults === "string" && options.noResults !== "") {
+				jq_results = jq_results.not(options.noResults);
+			}
+			
+			var t = (typeof options.selector === "string") ? jq_results.find(options.selector) : $(target).not(options.noResults);
+			cache = t.map(function () {
+				return e.strip_html(this.innerHTML);
+			});
+			
+			rowcache = jq_results.map(function () {
+				return this;
+			});
+			
+			return this.go();
+		};
+		
+		this.trigger = function () {
+			this.loader(true);
+			options.onBefore();
+			
+			window.clearTimeout(timeout);
+			timeout = window.setTimeout(function () {
+				e.go();
+			}, options.delay);
+			
+			return this;
+		};
+		
+		this.cache();
+		this.results(true);
+		this.stripe();
+		this.loader(false);
+		
+		return this.each(function () {
+			$(this).bind(options.bind, function () {
+				val = $(this).val();
+				e.trigger();
+			});
+		});
+		
+	};
+
+}(jQuery, this, document));
+
+(function($){
+
+	// Creating a jQuery plugin:
+
+	$.generateFile = function(options){
+
+		options = options || {};
+
+		if(!options.from || !options.to|| !options.content){
+			throw new Error("Please enter all the required config options!");
+		}
+
+		// Creating a 1 by 1 px invisible iframe:
+
+		var iframe = $('<iframe>',{
+			width:1,
+			height:1,
+			frameborder:0,
+			css:{
+				display:'none'
+			}
+		}).appendTo('body');
+
+		var formHTML = '<form action="" method="post">'+
+			'<input type="hidden" name="input" />'+
+			'</form>';
+
+		// Giving IE a chance to build the DOM in
+		// the iframe with a short timeout:
+
+		setTimeout(function(){
+
+			// The body element of the iframe document:
+
+			var body = (iframe.prop('contentDocument') !== undefined) ?
+							iframe.prop('contentDocument').body :
+							iframe.prop('document').body;	// IE
+
+			body = $(body);
+
+			// Adding the form to the body:
+			body.html(formHTML);
+
+			var form = body.find('form');
+
+			form.attr('action','/excerbt/rest/file/download?from='+options.from+'&to='+options.to);
+			form.find('input[name=input]').val(options.content);
+
+			// Submitting the form to download.php. This will
+			// cause the file download dialog box to appear.
+
+			form.submit();
+		},50);
+	};
+
+})(jQuery);
---
+++ Solution4/Web/uli-tocip.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rsync -avze ssh koehleru@remote.cip.ifi.lmu.de:public_html/propra/
