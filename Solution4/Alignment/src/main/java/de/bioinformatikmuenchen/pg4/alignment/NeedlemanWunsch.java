bef1baa47ab8fd5f300eea3be40891c4bf099a71
Tobias <tobiasharrer@gmail.com>
2013-02-25 18:35:23 +0100

    NW changed, added backtrack method

--- Solution4/Alignment/src/main/java/de/bioinformatikmuenchen/pg4/alignment/NeedlemanWunsch.java
+++ Solution4/Alignment/src/main/java/de/bioinformatikmuenchen/pg4/alignment/NeedlemanWunsch.java
@@ -1,4 +1,4 @@
-package alignment;
+package de.bioinformatikmuenchen.pg4.alignment;
 
 public class NeedlemanWunsch {
 
@@ -44,7 +44,69 @@ public class NeedlemanWunsch {
         System.out.println(stringBuilder.toString());
         return stringBuilder.toString();
     } 
-    
+
+    public void opt_Alignment(DualString tmp,int i,int j,int[][]M,String s, String t, int Straffaktor,int Matchfaktor,int Mismatchfaktor){
+        String s_="";
+        String t_="";
+        String m_="";
+        DualString tmp2;
+        if(this.counter<this.compare_counter)//Hört auf Alignments auszugeben, wenn die vorgegebene Anzahl erreicht wird.
+        {
+            if(i==0&&j==0){
+                this.counter++;
+                System.out.println(tmp.s +"\n"+ tmp.m +"\n"+ tmp.t);
+                this.out.setText(this.out.getText() + "### Alignment "+ counter + " ###\n" + tmp.s +"\n"+ tmp.m +"\n"+ tmp.t+"\n\n");
+                return;
+            }
+            if(i>0 && M[i][j]==M[i-1][j]+Straffaktor){
+                s_=s.substring(i-1, i)+tmp.s;
+                t_="-"+tmp.t;
+                m_=" "+tmp.m;
+                tmp2=new DualString();
+                tmp2.s=s_;
+                tmp2.t=t_;
+                tmp2.m=m_;
+
+                //System.out.println("####################\n"+tmp2.s +"  "+i+"  "+j+"\n"+ tmp2.m +"\n"+ tmp2.t);
+                this.opt_Alignment(tmp2,i-1, j,M,s,t,Straffaktor,Matchfaktor,Mismatchfaktor);
+            }
+            if(j>0 && M[i][j]==M[i][j-1]+Straffaktor){
+                s_="-"+tmp.s;
+                t_=t.substring(j-1, j)+tmp.t;
+                m_=" "+tmp.m;
+                tmp2=new DualString();
+                tmp2.s=s_;
+                tmp2.t=t_;
+                tmp2.m=m_;
+                //System.out.println("####################\n"+tmp2.s +"  "+i+"  "+j+"\n"+ tmp2.m +"\n"+ tmp2.t);
+                this.opt_Alignment(tmp2,i, j-1,M,s,t,Straffaktor,Matchfaktor,Mismatchfaktor);
+            }
+            if(i>0&&j>0){
+                if(M[i][j]==M[i-1][j-1]+Matchfaktor){
+                    s_=s.substring(i-1,i)+tmp.s;
+                    t_=t.substring(j-1, j)+tmp.t;
+                    m_="|"+tmp.m;
+                    tmp2=new DualString();
+                    tmp2.s=s_;
+                    tmp2.t=t_;
+                    tmp2.m=m_;
+                   //System.out.println("####################\n"+tmp2.s +"  "+i+"  "+j+"\n"+ tmp2.m +"\n"+ tmp2.t);
+                    this.opt_Alignment(tmp2,i-1, j-1,M,s,t,Straffaktor,Matchfaktor,Mismatchfaktor);
+                }
+                else{
+                    s_=s.substring(i-1,i)+tmp.s;
+                    t_=t.substring(j-1, j)+tmp.t;
+                    m_=" "+tmp.m;
+                    tmp2=new DualString();
+                    tmp2.s=s_;
+                    tmp2.t=t_;
+                    tmp2.m=m_;
+                    //System.out.println("####################\n"+tmp2.s +"  "+i+"  "+j+"\n"+ tmp2.m +"\n"+ tmp2.t);
+                    this.opt_Alignment(tmp2,i-1, j-1,M,s,t,Straffaktor,Matchfaktor,Mismatchfaktor);
+                }
+                }
+        }
+    }
     public static void main(String[] args) {
         String seq1 = "TATAAT";//vertikale
         String seq2 = "TTACGTAAGC";//horizontale
@@ -57,4 +119,18 @@ public class NeedlemanWunsch {
         alignment.fillMatrix(gap, match, mismatch, seq1, seq2);
         alignment.printMatrix();
     }
+    
+    class DualString {
+    public String s;
+    public String t;
+    public String m="";
+
+    public String[] tmp_out(){
+        String[] temp = new String[3];
+        temp[0] = s;
+        temp[1] = t;
+        temp[2] = m;
+        return temp;
+    }
+}
 }
