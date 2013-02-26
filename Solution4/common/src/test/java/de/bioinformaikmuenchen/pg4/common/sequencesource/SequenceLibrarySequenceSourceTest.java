ca4d49f74ba5a8ab43c1c8d7b87b8178d2617868
Uli Köhler <ulikoehler@online.de>
2013-02-26 15:53:28 +0100

    Seqlib src works now

---
+++ Solution4/common/src/test/java/de/bioinformaikmuenchen/pg4/common/sequencesource/SequenceLibrarySequenceSourceTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bioinformaikmuenchen.pg4.common.sequencesource;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.zip.GZIPInputStream;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author koehleru
+ */
+public class SequenceLibrarySequenceSourceTest extends TestCase {
+
+    public SequenceLibrarySequenceSourceTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSequence method, of class SequenceLibrarySequenceSource.
+     */
+    @Test
+    public void testGetSequence() throws IOException {
+        Reader reader = new InputStreamReader(new GZIPInputStream(
+                SequenceLibrarySequenceSource.class.getResourceAsStream("/de/bioinformatikmuenchen/pg4/common/sequencesource/domains.seqlib.gz")));
+        SequenceLibrarySequenceSource src = new SequenceLibrarySequenceSource(reader);
+        //Seq from the beginning
+        assertEquals(src.getSequence("12gsB02").getSequence(), "YGKDQQEAALVDMVNDGVEDLRCKYISLIYTNYEAGKDDYVKALPGQLKPFETLLSQNQGGKTFIVGDQISFADYNLLDLLLIHEVLAPGCLDAFPLLSAYVGRLSA");
+        assertEquals(src.getSequence("1a0aB00").getSequence(), "MKRESHKHAEQARRNRLAVALHELASLIPAEWKQQNVSAAPSKATTVEAACRYIRHLQQNGS");
+        //Somewhere in the middle
+        assertEquals(src.getSequence("1nltA02").getSequence(), "CKECEGRGGKKGAVKKCTSCNGQGIKFVTRQMGPMIQRFQTECDVCHGTGDIIDPKDRCKSCNGK");
+        assertEquals(src.getSequence("1njiC02").getSequence(), "GRRIQGQRRGRGTSTFRAPSHRYKADLEHRKVEDGDVIAGTVVDIEHDPARSAPVAAVEFEDGDRRLILAPEGVGVG");
+        //at the end
+        assertEquals(src.getSequence("7gatA00").getSequence(), "MKNGEQNGPTTCTNCFTQTTPVWRRNPEGQPLCNACGLFLKLHGVVRPLSLKTDVIKKRNRNSAN");
+        assertEquals(src.getSequence("8ickA01").getSequence(), "ETLNGGITDMLTELANFEKNVSQAIHKYNAYRKAASVIAKYPHKIKSGAEAKKLPGVGTKIAEKIDEFLATGKLRKLEKIRQ");
+        
+    }
+}
