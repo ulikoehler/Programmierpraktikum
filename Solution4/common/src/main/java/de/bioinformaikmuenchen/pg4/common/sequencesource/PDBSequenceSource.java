b5874c4979b91ab4f0589e81811acec0c00ec1f0
Uli Köhler <ulikoehler@online.de>
2013-02-26 15:36:01 +0100

    PDB seq src works now

--- Solution4/common/src/main/java/de/bioinformaikmuenchen/pg4/common/sequencesource/PDBSequenceSource.java
+++ Solution4/common/src/main/java/de/bioinformaikmuenchen/pg4/common/sequencesource/PDBSequenceSource.java
@@ -4,10 +4,54 @@
  */
 package de.bioinformaikmuenchen.pg4.common.sequencesource;
 
+import de.bioinformatikmuenchen.pg4.common.Sequence;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
+
 /**
  *
  * @author koehleru
  */
-public class PDBSequenceSource {
-    
+public class PDBSequenceSource implements ISequenceSource {
+
+    public Sequence getSequence(String id) {
+        try {
+            assert id.length() == 4;
+            //////////////////////
+            //////////////////////
+            //NOTE: Only the first sequence is being processed. All further sequences are being ignored silently!
+            //////////////////////
+            //////////////////////
+            //Setup the retriever
+            URL url = new URL("http://www.rcsb.org/pdb/files/fasta.txt?structureIdList=" + id);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setConnectTimeout(1000);
+            urlConnection.setReadTimeout(1000);
+            //Download the sequence lines (in FASTA format9
+            List<String> lines = IOUtils.readLines(urlConnection.getInputStream());
+            //Build the sequence (see note above) from the FASTA
+            StringBuilder builder = new StringBuilder();
+            for(String line : lines) {
+                //Process FASTA headline
+                if(line.startsWith(">")) {
+                    //Skip headline
+                    //break if this is the second headline <=> first seq has already been read
+                    if(builder.length() == 0) {
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
+                //Handle data lines
+                builder.append(line);
+            }
+            return new Sequence(id, builder.toString());
+        } catch (IOException ex) {throw new RuntimeException(ex);
+        }
+    }
 }
