b254bbfed7d81a8a1b551d5842e68c22c1df22da
Uli Köhler <ulikoehler@online.de>
2013-02-26 12:18:06 +0100

    Stuff

---
+++ Solution4/Alignment/src/main/java/de/bioinformatikmuenchen/pg4/alignment/recursive/io/AlignmentOutputFormatFactory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bioinformatikmuenchen.pg4.alignment.recursive.io;
+
+import de.bioinformatikmuenchen.pg4.alignment.AlignmentOutputFormat;
+
+/**
+ *
+ * @author koehleru
+ */
+public class AlignmentOutputFormatFactory {
+    public static IAlignmentOutputFormatter factorize(AlignmentOutputFormat format) {
+        
+    }
+}
