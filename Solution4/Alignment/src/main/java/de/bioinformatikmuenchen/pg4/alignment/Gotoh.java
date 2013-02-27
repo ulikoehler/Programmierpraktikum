0beedddea93e60a8f6a676e1f38a08cac5868da6
Uli Köhler <ulikoehler@online.de>
2013-02-27 15:28:57 +0100

    Added algorithmic stuff

--- Solution4/Alignment/src/main/java/de/bioinformatikmuenchen/pg4/alignment/Gotoh.java
+++ Solution4/Alignment/src/main/java/de/bioinformatikmuenchen/pg4/alignment/Gotoh.java
@@ -4,14 +4,30 @@
  */
 package de.bioinformatikmuenchen.pg4.alignment;
 
+import de.bioinformatikmuenchen.pg4.alignment.gap.ConstantGapCost;
+import de.bioinformatikmuenchen.pg4.alignment.gap.IGapCost;
+import de.bioinformatikmuenchen.pg4.alignment.io.IAlignmentOutputFormatter;
 import de.bioinformatikmuenchen.pg4.common.Sequence;
 import de.bioinformatikmuenchen.pg4.common.alignment.AlignmentResult;
+import de.bioinformatikmuenchen.pg4.common.distance.IDistanceMatrix;
 
 /**
  *
  * @author koehleru
  */
 public class Gotoh extends AlignmentProcessor {
+
+    public Gotoh(AlignmentMode mode, AlignmentAlgorithm algorithm, IDistanceMatrix distanceMatrix, IGapCost gapCost) {
+        super(mode, algorithm, distanceMatrix, gapCost);
+        assert gapCost instanceof ConstantGapCost;
+        //AlignmentResult result = new AlignmentResult();
+    }
+
+    public Gotoh(AlignmentMode mode, AlignmentAlgorithm algorithm, IDistanceMatrix distanceMatrix, IGapCost gapCost, IAlignmentOutputFormatter outputFormatter) {
+        super(mode, algorithm, distanceMatrix, gapCost, outputFormatter);
+        assert gapCost instanceof ConstantGapCost : "Classic Needleman Wunsch can't use affine gap cost";
+        assert algorithm == AlignmentAlgorithm.NEEDLEMAN_WUNSCH;
+    }
 
     @Override
     public AlignmentResult align(Sequence seq1, Sequence seq2) {
