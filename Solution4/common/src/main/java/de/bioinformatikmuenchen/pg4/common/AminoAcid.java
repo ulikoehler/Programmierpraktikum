ff60fd11e9c45f495d7ccad216f2994f130f9e0a
Yann Spöri <yann_spoeri@web.de>
2013-02-27 15:40:44 +0100

    stuff

--- Solution4/common/src/main/java/de/bioinformatikmuenchen/pg4/common/AminoAcid.java
+++ Solution4/common/src/main/java/de/bioinformatikmuenchen/pg4/common/AminoAcid.java
@@ -10,30 +10,10 @@ public class AminoAcid {
      * one letter code of all AA
      */
     public static char[] AC1 = {
-        'A', 'R', 'N', 'D', 'B', 'C',
-        'E', 'Q', 'Z', 'G', 'H', 'I',
-        'L', 'K', 'M', 'F', 'P', 'S',
-        'T', 'W', 'Y', 'V'
-    };
-    
-    /**
-     * three letter code of each AA
-     */
-    public static String[] AC3 = {
-        "ala", "arg", "asn", "asp", "asx", "cys",
-        "glu", "gln", "glx", "gly", "his", "ile",
-        "leu", "lys", "met", "phe", "pro", "ser",
-        "thr", "trp", "tyr", "val"
-    };
-    
-    /**
-     * full names of each AA
-     */
-    public static String[] ACf = {
-        "alanine", "arginine", "asparagine", "aspartic acid", "asparagine or aspartic acid", "cysteine",
-        "glutamic acid", "glutamine", "glutamine or glutamic acid", "glycine", "histidine", "isoleucine",
-        "leucine", "lysine", "methionine", "phenylalanine", "proline", "serine",
-        "threonine", "tryptophan", "tyrosine", "valine"
+        'A', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'K', 'L',
+        'M', 'N', 'P', 'Q', 'R',
+        'S', 'T', 'V', 'W', 'Y',
     };
     
 }
