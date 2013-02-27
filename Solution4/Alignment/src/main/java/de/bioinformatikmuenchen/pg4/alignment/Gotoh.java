addc372ab82538a05cc38ea780ba85552e21a309
Uli Köhler <ulikoehler@online.de>
2013-02-27 15:11:49 +0100

    Add gotoh

---
+++ Solution4/Alignment/src/main/java/de/bioinformatikmuenchen/pg4/alignment/Gotoh.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bioinformatikmuenchen.pg4.alignment;
+
+import de.bioinformatikmuenchen.pg4.common.Sequence;
+import de.bioinformatikmuenchen.pg4.common.alignment.AlignmentResult;
+
+/**
+ *
+ * @author koehleru
+ */
+public class Gotoh extends AlignmentProcessor {
+
+    @Override
+    public AlignmentResult align(Sequence seq1, Sequence seq2) {
+        return null;
+    }
+}
