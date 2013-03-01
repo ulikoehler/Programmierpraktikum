d1beea1252aaf729aa258f6c5f569e2dafef40f1
Uli Köhler <ulikoehler@online.de>
2013-03-01 10:53:44 +0100

    Stuff

---
+++ Solution4/Alignment/src/main/java/de/bioinformatikmuenchen/pg4/alignment/AlignmentException.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bioinformatikmuenchen.pg4.alignment;
+
+/**
+ *
+ * @author koehleru
+ */
+public class AlignmentException extends Exception {
+
+    /**
+     * Creates a new instance of
+     * <code>AlignmentException</code> without detail message.
+     */
+    public AlignmentException() {
+    }
+
+    /**
+     * Constructs an instance of
+     * <code>AlignmentException</code> with the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public AlignmentException(String msg) {
+        super(msg);
+    }
+    
+    
+    public AlignmentException(String msg, Exception cause) {
+        super(msg, cause);
+    }
+}
