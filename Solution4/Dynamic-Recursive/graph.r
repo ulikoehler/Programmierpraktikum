d7b4468680d9641f1031f4bfdece7b650e28e28b
Uli Köhler <ulikoehler@online.de>
2013-03-07 22:04:35 +0100

    Stuff

--- Solution4/Dynamic-Recursive/graph.r
+++ Solution4/Dynamic-Recursive/graph.r
@@ -35,7 +35,7 @@ close(con)
 #df1 <- transform(df1, Algorithm = reorder(Algorithm, Index))
 library(ggplot2)
 
-png(file="rec.png",height=500,width=700)
+png(file="rec.png",height=300,width=500)
 
 
   print(ggplot(data=df1, aes(x=Größe, y=Laufzeit, group=Algorithmus, colour=Algorithmus)) +
